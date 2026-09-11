74fb02afe369410230d20fc588e8f8834f07b198
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2026-09-11 10:14:53 +0000

    TEST 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const relativeList = ["Chacha", "Chachi", "Bua", "Mashi", "Tau"]
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key={relativeList}>
+        {relativeList.map((item, index) => {
+          return <li key={`relativeListItem${index + 1}`}>{item}</li>
+        })}
+      </ol>
     </div>
   )
 }
